ed8dcf86739a068cca850245368e3994388f7924
Unk0wn1337 <kelogames@gmail.com>
2024-12-09 09:18:12 +0100

    f1 2 b

--- src/main/java/hu/szamalk/Tabla.java
+++ src/main/java/hu/szamalk/Tabla.java
@@ -4,7 +4,8 @@ public class Tabla {
     private char[][] t;
     private char uresCella;
 
-    public Tabla() {
+    public Tabla(char uresCella) {
         this.t = new char[8][8];
+        this.uresCella = uresCella;
     }
 }
